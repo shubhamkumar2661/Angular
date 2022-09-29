75c328dfce83c5b36b521222fdca1482aa9eefc9
go <e@g.coo>
2022-09-29 18:54:17 +0530

    Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'smit';
+  count = 0;
+
+  number = 2;
+  Number = 1;
+  listOfNumbers = [12,43,543,53,1];
+  listOfName =["abc","def","ghi","jkl","mno"];
+  increment = () => {
+    this.count++;
+  }
+  decrease = () => {
+    this.count--;
+  }
 }
